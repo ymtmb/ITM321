09751d7c9f64b326a6fdcdd3091352ed38f12acd
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-12-05 23:01:47 -0600

    Delete Asia_Crimes.sql

--- Asia_Crimes.sql
+++
@@ -1,7 +0,0 @@
-USE GTD_Database;
-SELECT * FROM Asian_Crimes
-WHERE region_txt = "East Asia";
-SELECT * FROM Asian_Crimes
-WHERE region_txt = "Southeast Asia";
-SELECT * FROM Asian_Crimes
-WHERE region_txt = "South Asia";
